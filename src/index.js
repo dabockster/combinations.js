dcef6f8d9e48bf580d1736c082184aee3b1d66d5
Steven Bock <steven.bockster@gmail.com>
2015-04-21 00:17:02 -0700

    Finished writing the bulk of the library

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var mathjs = require('mathjs');
+
+var combinations = function(n, k){
+	//check if k <= n
+	if (k <= n){
+		return undefined;
+	}
+	//grab n! and k!
+	var nFact = mathjs.factorial(n);
+	var kFact = mathjs.factorial(k);
+
+	//set numerator & denominator
+	var numerator = nFact;
+	var denominator = kFact * (nFact - kFact);
+
+	return numerator / denominator;
+};
+
+module.exports = combinations;
